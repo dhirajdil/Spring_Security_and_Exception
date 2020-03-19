43b9ac4922756ec8267b384634dffdddcc979bc6
dhiraj.kumar <dhiraj.kumar@HYD1WS0018.sonhydtui.local>
2020-03-19 18:32:34 +0530

    1st commit

---
+++ spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		UserBuilder users=User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser(users.username("dhiraj").password("test123").roles("EMPLOYEE"))
+		.withUser(users.username("MANISH").password("test123").roles("MANAGER"))
+		.withUser(users.username("MOHAN").password("test123").roles("ADMIN"));
+	}
+
+}
